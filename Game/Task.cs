f6901e51eeaae424edb3ff69af9f434e2487f2a8
Zuguki <guskov.zahar20@gmail.com>
2023-02-27 06:41:26 +0300

    TaskManager done

--- Game/Task.cs
+++ Game/Task.cs
@@ -8,6 +8,38 @@ public enum WeakDay
 }
 
 
+public class TaskManager
+{
+    public readonly List<Task> Tasks;
+
+    public TaskManager()
+    {
+        Tasks = new List<Task>();
+    }
+
+    public TaskManager Add(string name, WeakDay weakDay = WeakDay.Random)
+    {
+        Tasks.Add(new Task(name, weakDay));
+        return this;
+    }
+
+    public TaskManager RemoveByName(string name)
+    {
+        var task = GetTask(name);
+        Tasks.Remove(task);
+        return this;
+    }
+
+    private Task GetTask(string name)
+    {
+        var task = Tasks.FirstOrDefault(task => task.Name == name);
+        if (task is null)
+            throw new ArgumentException($"Task with {nameof(name)} {name} does't exist");
+
+        return task;
+    }
+}
+
 public class Task
 {
     public Task(string name)
