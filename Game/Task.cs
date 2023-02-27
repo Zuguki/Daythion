78a3131aa989e680ed7eecf12363027460af7616
Zuguki <guskov.zahar20@gmail.com>
2023-02-27 06:41:43 +0300

    Application was added

--- Game/Task.cs
+++ Game/Task.cs
@@ -7,6 +7,40 @@ public enum WeakDay
     Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Random
 }
 
+public class Application
+{
+    private Dictionary<WeakDay, List<Task>> _schedule;
+    private const int DaysInWeak = 7;
+
+    public Application()
+    {
+        _schedule = new Dictionary<WeakDay, List<Task>>();
+        TaskManager = new TaskManager();
+    }
+    
+    public TaskManager TaskManager { get; private set; }
+
+    public void RandomizeSchedule()
+    {
+        var rnd = new Random();
+    }
+}
+
+public static class IntExt
+{
+    public static WeakDay GetWeakDay(this int number) =>
+        number switch
+        {
+            1 => WeakDay.Monday,
+            2 => WeakDay.Tuesday,
+            3 => WeakDay.Wednesday,
+            4 => WeakDay.Thursday,
+            5 => WeakDay.Friday,
+            6 => WeakDay.Saturday,
+            7 => WeakDay.Sunday,
+            _ => throw new ArgumentException()
+        };
+}
 
 public class TaskManager
 {
